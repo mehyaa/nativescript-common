240c09ecac762db6787f8df1fd0cceafe5a40548
mehyaa <mehyaa@gmail.com>
2019-04-11 11:38:30 +0300

    Ad mixin interstitial support

--- mixins/ad-mixin.js
+++ mixins/ad-mixin.js
@@ -1,22 +1,58 @@
 import * as adService from '../services/ad-service';
 
 const mixin = {
+    data() {
+        return {
+            adType: null
+        };
+    },
+
+    computed: {
+        adBannerOptions() {
+            const vm = this;
+
+            return vm.config && vm.config.admob;
+        },
+
+        adInterstitialOptions() {
+            const vm = this;
+
+            return vm.config && vm.config.admob;
+        }
+    },
+
     mounted() {
         const vm = this;
 
-        setTimeout(() => {
-            adService
-                .showBanner(vm.config.admob)
-                .catch(error => vm.eventBus.send('error', { error, context: vm }));
-        }, 500);
+        switch (vm.adType) {
+            case 'banner':
+                setTimeout(() => {
+                    adService
+                        .showBanner(vm.adBannerOptions)
+                        .catch(error => vm.eventBus.send('error', { error, context: vm }));
+                }, 500);
+
+                break;
+
+            case 'interstitial':
+                setTimeout(() => {
+                    adService
+                        .showInterstitial(vm.adInterstitialOptions)
+                        .catch(error => vm.eventBus.send('error', { error, context: vm }));
+                }, 500);
+
+                break;
+        }
     },
 
     beforeDestroy() {
         const vm = this;
 
-        adService
-            .hideBanner()
-            .catch(error => vm.eventBus.send('error', { error, context: vm }));
+        if (vm.adType === 'banner') {
+            adService
+                .hideBanner()
+                .catch(error => vm.eventBus.send('error', { error, context: vm }));
+        }
     }
 }
 
